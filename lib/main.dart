763f2bcc66e2bf5cf30ca5967d080ffaa98baee7
Hamza Baloch <hamzaqaisrani246@gmail.com>
2025-07-07 16:41:44 +0500

    created splaash screen

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+
+void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    initialization();
+  }
+
+  void initialization() async{
+    log("ready 2");
+    await Future.delayed(Duration(seconds: 1));
+    log("ready 1");
+    await Future.delayed(Duration(seconds: 1));
+    log("GO!");
+    FlutterNativeSplash.remove();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(body: Center(child: Text("Hello World"))));
+  }
+}
